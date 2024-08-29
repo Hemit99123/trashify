d85b16b9ec0097329255dc441393ebba4438e986
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-08-29 12:21:43 -0400

    added a refresh btn to reset search results

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import SearchBar from '@/components/SearchBar';
 import usePostStore from '@/store/usePostStore';
 import PostView from '@/components/PostView';
+import Refresh from '@/assets/regular/refresh.svg'
 
 const HomeView = () => {
   const setPosts = usePostStore((state) => state.setState)
@@ -29,29 +30,32 @@ const HomeView = () => {
     getCurrentPosition();
   }, []);
 
-  useEffect(() => {
-    const handleGetPosts = async () => {
-      if (lat !== null && long !== null) {
-        try {
-          const result = await axios.get(`/api/post`, { params: { lat, long } });
-          console.log('Fetched posts:', result.data);
-          setPosts(result.data.data);
-        } catch (error) {
-          console.error('Error fetching posts:', error);
-        }
+  const handleGetPosts = async () => {
+    if (lat !== null && long !== null) {
+      try {
+        const result = await axios.get(`/api/post`, { params: { lat, long } });
+        console.log('Fetched posts:', result.data);
+        setPosts(result.data.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
       }
-    };
+    }
+  };
 
+  useEffect(() => {
     handleGetPosts();
-  }, [lat, long]);
-
-
-
-  console.log('updated')
+  }, [lat, long, handleGetPosts]);
 
   return (
     <div className='flex flex-col items-center'>
       <SearchBar />
+      <button 
+        className="mt-3 flex items-center border-2 border-green-600 text-green-800 text-lg font-bold rounded-xl px-3 py-2" 
+        onClick={handleGetPosts}
+      >
+        <Refresh />
+        Refresh
+      </button>
       <hr className="w-full h-px my-5 bg-gray-300" />
       <div className='container mx-auto px-4 lg:px-16'>
         <PostView />
