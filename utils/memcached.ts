967cf5d7d20607f3ae20d1944d0a3a2a87e2826b
Hemit Patel <patelvhemit@gmail.com>
2024-08-30 11:24:50 -0400

    another refactor of exports

--- utils/memcached.ts
+++ utils/memcached.ts
@@ -1,5 +1,7 @@
 import Memcached from "memcached";
 
-export const memcached = new Memcached('localhost:11211');
-export const CACHE_CITY_EXPIRES = 60 * 60; // Cache expiration time in 3600 seconds (1 hour)
-export const CACHE_BOUNDS_EXPIRES = 60*60*24*30 // Same logic but for a longer time period of 30 days (expressed in days for most accuracy)
+const memcached = new Memcached('localhost:11211'); 
+const CACHE_CITY_EXPIRES = 60 * 60; // Cache expiration time in 3600 seconds (1 hour)
+const CACHE_BOUNDS_EXPIRES = 60*60*24*30 // Same logic but for a longer time period of 30 days (expressed in days for most accuracy)
+
+export { memcached, CACHE_CITY_EXPIRES, CACHE_BOUNDS_EXPIRES }
