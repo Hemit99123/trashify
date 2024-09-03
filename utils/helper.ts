6930ec43493d4161773acee41fd905569897cb60
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-02 21:00:45 -0400

    fixed some code in helper.ts

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,7 +1,5 @@
 const sanitizeKey = (key: string): string => key.replace(/\s+/g, '_').trim();
 
-export const TYPEOF_STRING_ERROR_MESSAGE = "Invalid string value";
-
 export const NAV_ITEMS = [
   {title: "Home", target_path: "/"},
   {title: "Post", target_path: "/post"},
@@ -11,17 +9,5 @@ export const NAV_ITEMS = [
 
 const TYPEOF_STRING_ERROR_MESSAGE = "Invalid string value";
 
-// Dynamically import the Leaflet icon on the client side
-const customIcon = dynamic(
-  () => import('leaflet').then((module) =>
-    module.icon({
-      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-    })
-  ),
-  { ssr: false } // Disable server-side rendering for this module
-);
 
-export { sanitizeKey, TYPEOF_STRING_ERROR_MESSAGE, customIcon };
+export { sanitizeKey, TYPEOF_STRING_ERROR_MESSAGE };
